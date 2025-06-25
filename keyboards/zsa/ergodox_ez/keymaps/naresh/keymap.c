aa05837bd5f8923b860a65a6cbe69ea0ba8ca668
Nareshkumar Rao <dev@nrx.sh>
2025-06-25 16:24:33 +0200

    add toggle for Auto-Shift

--- keyboards/zsa/ergodox_ez/keymaps/naresh/keymap.c
+++ keyboards/zsa/ergodox_ez/keymaps/naresh/keymap.c
@@ -1,3 +1,5 @@
+// clang-format off
+
 #include QMK_KEYBOARD_H
 #include "version.h"
 #include "i18n.h"
@@ -18,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DELETE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(1),                                          LALT(KC_TAB),   KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
     KC_BSPC,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           LT(2, KC_SCLN), KC_QUOTE,
     KC_LEFT_SHIFT,  MT(MOD_LCTL, KC_Z),KC_X,           KC_C,           KC_V,           KC_B,           KC_HYPR,                                        KC_MEH,         KC_N,           KC_M,           KC_COMMA,       KC_DOT,         MT(MOD_RCTL, KC_SLASH),KC_RIGHT_SHIFT,
-    KC_GRAVE,       LSFT(KC_LEFT_CTRL),KC_TRANSPARENT, KC_LEFT_ALT,    MO(1),                                                                                                          MO(1),          KC_TRANSPARENT, KC_TRANSPARENT, LSFT(KC_RIGHT_CTRL),KC_TRANSPARENT,
+    KC_GRAVE,       LSFT(KC_LEFT_CTRL),KC_TRANSPARENT, KC_LEFT_ALT,    MO(1),                                                                                                          MO(1),          KC_TRANSPARENT, KC_TRANSPARENT, LSFT(KC_RIGHT_CTRL),AS_TOGG,
                                                                                                     KC_ESCAPE,      KC_TRANSPARENT, KC_LEFT_ALT,    MT(MOD_LCTL, KC_ESCAPE),
                                                                                                                     KC_HOME,        KC_PAGE_UP,
                                                                                     LT(2, KC_SPACE),KC_BSPC,        KC_END,         KC_PGDN,        MT(MOD_LGUI, KC_TAB),KC_ENTER
